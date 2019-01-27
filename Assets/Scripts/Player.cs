0bfc00074839fe3ea9d7a843518a102f8694ce10
mightyDavros <davidwollen@gmail.com>
2019-01-27 17:08:36 +0000

    First Commit - just added player horizontal movement - lecture 33

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigid;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get reference to rigidbody
+        _rigid = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check for 
+        //horizontal input for left/right
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        
+        _rigid.velocity = new Vector2(horizontalInput, _rigid.velocity.y);
+
+
+    }
+}
